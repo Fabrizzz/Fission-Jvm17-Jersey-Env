60f5422c86d16d4578207d57f8923e70505a9e81
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-05-15 17:53:42 +0200

    template updated to java17 and maven 3.9.1

--- jvm-jersey/src/main/java/io/fission/Main.java
+++ jvm-jersey/src/main/java/io/fission/Main.java
@@ -1,43 +1,68 @@
 package io.fission;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HotSwapHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
 	private static final int DEFAULT_PORT = 8888;
-	
-	private int serverPort;
-	
-	public Main(int serverPort) throws Exception {
-		this.serverPort = serverPort;
-		Server server = configureServer();	        
-        server.start();
-        server.join();
-	}	
 
-	private Server configureServer() {
-		ResourceConfig resourceConfig = new ResourceConfig();		
-		resourceConfig.packages(JerseyServer.class.getPackage().getName());
-		resourceConfig.register(JacksonFeature.class);
-		ServletContainer servletContainer = new ServletContainer(resourceConfig);
-		ServletHolder sh = new ServletHolder(servletContainer);
-		Server server = new Server(serverPort);		
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        context.addServlet(sh, "/*");
-		server.setHandler(context);
-		return server;
+	static HotSwapHandler hotSwapHandler;
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public Main(int serverPort) throws Exception {
+		Server server = new Server(serverPort);
+
+		ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+		server.setHandler(contextHandlerCollection);
+
+		hotSwapHandler = new HotSwapHandler();
+		ContextHandler contextHandler = new ContextHandler();
+		contextHandler.setHandler(new DefaultHandler());
+		contextHandler.setContextPath("/");
+		hotSwapHandler.setHandler(contextHandler);
+		contextHandlerCollection.addHandler(hotSwapHandler);
+
+		ResourceConfig config = new ResourceConfig();
+		config.register(RequestHandler.class);
+		// Create a ServletContainer with the ResourceConfig
+		ServletContainer jerseyServletContainer = new ServletContainer(config);
+		// Create a ServletHolder with the ServletContainer
+		ServletHolder servletHolder = new ServletHolder(jerseyServletContainer);
+		// Create a new ServletContextHandler and add the ServletHolder to it
+		ServletContextHandler jettyServletContext = new ServletContextHandler();
+		jettyServletContext.setContextPath("/v2"); // Set the context path for the Jersey resource
+		jettyServletContext.addServlet(servletHolder, "/*");
+		// Add the new ServletContextHandler to the ContextHandlerCollection
+		contextHandlerCollection.addHandler(jettyServletContext);
+
+		// Start the server
+		server.start();
+		logger.info(server.getURI().toString());
+		Handler[] handlers = contextHandlerCollection.getHandlers();
+		for (Handler handler : handlers) {
+			if (handler instanceof ContextHandler contextHandle) {
+				String contextPath = contextHandle.getContextPath();
+				logger.info("Entry point: " + contextPath);
+			}
+		}
 	}
-	
+
+
 	public static void main(String[] args) throws Exception {
-		
+
 		int serverPort = DEFAULT_PORT;
-		
+
 		if(args.length >= 1) {
 			try {
 				serverPort = Integer.parseInt(args[0]);
@@ -45,6 +70,6 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
-		new Main(serverPort);	
+		new Main(serverPort);
 	}
 }
---
+++ jvm-jersey/src/main/java/io/fission/RequestHandler.java
@@ -0,0 +1,30 @@
+package io.fission;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import static io.fission.Main.hotSwapHandler;
+
+
+@Path("specialize")
+public class RequestHandler {
+    @Consumes("application/json")
+    @POST
+    public Response specialize(FunctionLoadRequest request) {
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setWar(request.getFilepath());
+        webAppContext.setExtractWAR(true);
+        try {
+            hotSwapHandler.setHandler(webAppContext);
+        } catch (Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+        if (!webAppContext.isAvailable()){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok().build();
+    }
+}
